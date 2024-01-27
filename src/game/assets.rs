70dcfbb7f2c2132b64911615a8ae8c02ae508277
passivedragon <jax@passivedragon.org>
2024-01-27 17:02:02 +0100

    add dev dependencies to readme and fix asset path

--- src/game/assets.rs
+++ src/game/assets.rs
@@ -3,6 +3,8 @@ use bevy_asset_loader::asset_collection::AssetCollection;
 
 #[derive(AssetCollection, Resource)]
 pub(crate) struct MyAssets {
-    #[asset(path = "house.gltf")]
+    #[asset(path = "house/house.gltf")]
     garage_handle: Handle<Mesh>,
+    #[asset(path = "garage musician default.gltf")]
+    musician_default: Handle<Mesh>,
 }
