fa04783df38322f5ce6bbc51c8f94a7805d542a1
passivedragon <jax@passivedragon.org>
2024-01-28 17:24:44 +0100

    heatstroke snapshot

--- src/game/heatstroke.rs
+++ src/game/heatstroke.rs
@@ -9,7 +9,8 @@ use crate::AppState;
 pub struct HeastrokeResource{
     passed_out: f32,
     timer: Timer,
-    pub pedestal_id: Entity
+    pub pedestal_id: Entity,
+    pub musician_id: Entity,
 }
 
 impl Default for HeastrokeResource{
@@ -18,6 +19,7 @@ impl Default for HeastrokeResource{
             passed_out: 0.0,
             timer: Timer::new(Duration::from_secs(8), TimerMode::Once),
             pedestal_id: Entity::PLACEHOLDER,
+            musician_id: Entity::PLACEHOLDER,
         }
     }
 }
@@ -27,12 +29,18 @@ pub struct Heatstroke {
     // identify who
 }
 
-pub fn watch_heatstroke(mut studio: ResMut<FmodStudio>, mut tracker: ResMut<HeastrokeResource>, time: Res<Time>) {
+pub fn watch_heatstroke(mut studio: ResMut<FmodStudio>, mut heatstroke: EventReader<Heatstroke>, mut tracker: ResMut<HeastrokeResource>, time: Res<Time>) {
     // Slot1_A_Detune
     // let mut detune_param = studio.0.get_parameter_by_name("Slot1_A_Detune").expect("couldn't find paramter");
     // detune_param.1 = 1.0;
 
-    studio.0.set_parameter_by_name("Slot1_A_Detune", 0.5, true).expect("unable to set fmod parameter");
+    if heatstroke.is_empty() {
+        
+    }
+
+    // studio.0.set_parameter_by_name("Slot1_A_Detune", 0.5, true).expect("unable to set fmod parameter");
+
+    // info!("modulating fmod - heatstroke")
 }
 
 pub fn track_heatstroke(keys: Res<Input<KeyCode>>, mut event_writer: EventWriter<Heatstroke>) {
@@ -46,6 +54,8 @@ pub fn trigger_heatstroke(mut heatstroke: EventReader<Heatstroke>, state: Res<St
         println!("received heatstroke");
         if state.get() != &AppState::Game {
             println!("but not in game");
+            return;
         }
+
     }
 }
