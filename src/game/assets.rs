7c84e435d75348881fe6153e068ce131ea11a6ec
AlbertHahn <albert.post@mailbox.org>
2024-01-28 03:50:37 +0100

    added pedestal spawn

--- src/game/assets.rs
+++ src/game/assets.rs
@@ -7,8 +7,10 @@ pub struct MyAssets {
     pub garage_handle: Handle<Scene>,
     #[asset(path = "garage musician default.gltf#Mesh0")]
     pub musician_default: Handle<Mesh>,
+    #[asset(path = "garage musician default.gltf#Material0")]
+    pub musician_default_material: Handle<StandardMaterial>,
     #[asset(path = "pedestal.gltf#Mesh0")]
-    pub pedestal_handle: Handle<Scene>,
+    pub pedestal_handle: Handle<Mesh>,
     #[asset(path ="textures/facialexpressions/maleeyes1/", collection(typed))]
     pub eyes_healthy: Vec<Handle<Image>>,
     #[asset(path ="textures/facialexpressions/hurt1male", collection(typed))]
